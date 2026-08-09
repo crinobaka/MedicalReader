a17b048d4902993744597d1c7c5fcbb8dd6e684d
crinobaka <crinobakabaka@qq.com>
2026-08-09 13:42:04 +0800

    feaet(library): introduce document metadata foundation

--- lib/features/library/models/library_document.dart
+++ lib/features/library/models/library_document.dart
@@ -1,25 +1,43 @@
 import '../../../core/file_manager/models/document_file.dart';
 
 class LibraryDocument {
+  final String id;
+
   final DocumentFile file;
 
   final String title;
 
+  final int? pages;
+
+  final Map<String, dynamic> metadata;
+
   final DateTime addedAt;
 
   const LibraryDocument({
+    required this.id,
+
     required this.file,
 
     required this.title,
+
+    this.pages,
+
+    this.metadata = const {},
 
     required this.addedAt,
   });
 
   factory LibraryDocument.fromFile(DocumentFile file) {
     return LibraryDocument(
+      id: file.path,
+
       file: file,
 
       title: file.name.replaceAll('.pdf', ''),
+
+      pages: null,
+
+      metadata: {},
 
       addedAt: DateTime.now(),
     );
--- lib/features/library/widgets/document_card.dart
+++ lib/features/library/widgets/document_card.dart
@@ -2,38 +2,23 @@ import 'package:flutter/material.dart';
 
 import '../models/library_document.dart';
 
-
-
 class DocumentCard extends StatelessWidget {
-
-
   final LibraryDocument document;
 
-
-  const DocumentCard({
-    super.key,
-    required this.document,
-  });
-
-
+  const DocumentCard({super.key, required this.document});
 
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return ListTile(
+      title: Text(document.title),
 
-      title:
-          Text(
-            document.title,
-          ),
-
-      subtitle:
-          Text(
-            '${document.file.size} bytes',
-          ),
-
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('${document.file.size} bytes'),
+          if (document.pages != null) Text('${document.pages} pages'),
+        ],
+      ),
     );
-
   }
-
 }
