7328f7c8ec50931e21eb6384df7491492a4fe34f
crinobaka <crinobakabaka@qq.com>
2026-08-12 22:53:32 +0800

    package

--- core/medical_core/src/reader/document.rs
+++ core/medical_core/src/reader/document.rs
@@ -28,7 +28,15 @@ impl Document {
     }
 
     pub fn page_count(&self) -> Result<u32, Error> {
-        self.inner.page_count()
+        let page_count = self.inner.page_count()?;
+
+        if page_count < 0 {
+            return Err(Error::Generic(
+                "MuPDF returned a negative page count".to_string(),
+            ));
+        }
+
+        Ok(page_count as u32)
     }
 
     pub fn render_page(
--- lib/core/ffi/medical_core.dart
+++ lib/core/ffi/medical_core.dart
@@ -1,16 +1,148 @@
 import 'dart:ffi';
 import 'dart:io';
 
+import 'package:ffi/ffi.dart';
+
+final class _MedicalCoreDocument extends Opaque {}
+
+final class _MedicalCorePage extends Struct {
+  @Uint32()
+  external int width;
+
+  @Uint32()
+  external int height;
+
+  @Uint32()
+  external int stride;
+
+  @Uint8()
+  external int components;
+
+  external Pointer<Uint8> data;
+
+  @IntPtr()
+  external int dataLen;
+}
+
 typedef _MedicalCoreHelloNative = Int32 Function();
 typedef _MedicalCoreHelloDart = int Function();
+
+typedef _MedicalCoreOpenBookNative =
+    Pointer<_MedicalCoreDocument> Function(Pointer<Utf8>, Pointer<Utf8>);
+typedef _MedicalCoreOpenBookDart =
+    Pointer<_MedicalCoreDocument> Function(Pointer<Utf8>, Pointer<Utf8>);
+
+typedef _MedicalCoreCloseBookNative =
+    Void Function(Pointer<_MedicalCoreDocument>);
+typedef _MedicalCoreCloseBookDart =
+    void Function(Pointer<_MedicalCoreDocument>);
+
+typedef _MedicalCoreGetPageCountNative =
+    Int32 Function(Pointer<_MedicalCoreDocument>, Pointer<Uint32>);
+typedef _MedicalCoreGetPageCountDart =
+    int Function(Pointer<_MedicalCoreDocument>, Pointer<Uint32>);
+
+typedef _MedicalCoreRenderPageNative =
+    Pointer<_MedicalCorePage> Function(
+      Pointer<_MedicalCoreDocument>,
+      Uint32,
+      Uint32,
+    );
+typedef _MedicalCoreRenderPageDart =
+    Pointer<_MedicalCorePage> Function(Pointer<_MedicalCoreDocument>, int, int);
+
+typedef _MedicalCoreFreePageNative = Void Function(Pointer<_MedicalCorePage>);
+typedef _MedicalCoreFreePageDart = void Function(Pointer<_MedicalCorePage>);
+
+class MedicalCorePage {
+  final int width;
+  final int height;
+  final int stride;
+  final int components;
+  final List<int> data;
+
+  const MedicalCorePage({
+    required this.width,
+    required this.height,
+    required this.stride,
+    required this.components,
+    required this.data,
+  });
+}
+
+class MedicalCoreDocument {
+  final Pointer<_MedicalCoreDocument> _handle;
+  final MedicalCore _core;
+
+  bool _closed = false;
+
+  MedicalCoreDocument._({
+    required Pointer<_MedicalCoreDocument> handle,
+    required MedicalCore core,
+  }) : _handle = handle,
+       _core = core;
+
+  int get pageCount {
+    if (_closed) {
+      throw StateError('MedicalCoreDocument is already closed.');
+    }
+
+    return _core.getPageCount(_handle);
+  }
+
+  MedicalCorePage renderPage({required int pageIndex, required int dpi}) {
+    if (_closed) {
+      throw StateError('MedicalCoreDocument is already closed.');
+    }
+
+    return _core.renderPage(_handle, pageIndex: pageIndex, dpi: dpi);
+  }
+
+  void close() {
+    if (_closed) {
+      return;
+    }
+
+    _core.closeBook(_handle);
+    _closed = true;
+  }
+}
 
 class MedicalCore {
   MedicalCore._(this._library) {
     _hello = _library
-        .lookup<NativeFunction<_MedicalCoreHelloNative>>(
-          'medical_core_hello',
-        )
+        .lookup<NativeFunction<_MedicalCoreHelloNative>>('medical_core_hello')
         .asFunction<_MedicalCoreHelloDart>();
+
+    _openBook = _library
+        .lookup<NativeFunction<_MedicalCoreOpenBookNative>>(
+          'medical_core_open_book',
+        )
+        .asFunction<_MedicalCoreOpenBookDart>();
+
+    _closeBook = _library
+        .lookup<NativeFunction<_MedicalCoreCloseBookNative>>(
+          'medical_core_close_book',
+        )
+        .asFunction<_MedicalCoreCloseBookDart>();
+
+    _getPageCount = _library
+        .lookup<NativeFunction<_MedicalCoreGetPageCountNative>>(
+          'medical_core_get_page_count',
+        )
+        .asFunction<_MedicalCoreGetPageCountDart>();
+
+    _renderPage = _library
+        .lookup<NativeFunction<_MedicalCoreRenderPageNative>>(
+          'medical_core_render_page',
+        )
+        .asFunction<_MedicalCoreRenderPageDart>();
+
+    _freePage = _library
+        .lookup<NativeFunction<_MedicalCoreFreePageNative>>(
+          'medical_core_free_page',
+        )
+        .asFunction<_MedicalCoreFreePageDart>();
   }
 
   static MedicalCore? _instance;
@@ -19,12 +151,112 @@ class MedicalCore {
 
   late final _MedicalCoreHelloDart _hello;
 
+  late final _MedicalCoreOpenBookDart _openBook;
+
+  late final _MedicalCoreCloseBookDart _closeBook;
+
+  late final _MedicalCoreGetPageCountDart _getPageCount;
+
+  late final _MedicalCoreRenderPageDart _renderPage;
+
+  late final _MedicalCoreFreePageDart _freePage;
+
   factory MedicalCore() {
     return _instance ??= MedicalCore._(_openLibrary());
   }
 
   int hello() {
     return _hello();
+  }
+
+  MedicalCoreDocument openBook({required String id, required String path}) {
+    final idPointer = id.toNativeUtf8();
+    final pathPointer = path.toNativeUtf8();
+
+    try {
+      final handle = _openBook(idPointer, pathPointer);
+
+      if (handle == nullptr) {
+        throw StateError('Failed to open document: $path');
+      }
+
+      return MedicalCoreDocument._(handle: handle, core: this);
+    } finally {
+      calloc.free(idPointer);
+      calloc.free(pathPointer);
+    }
+  }
+
+  void closeBook(Pointer<_MedicalCoreDocument> handle) {
+    if (handle == nullptr) {
+      return;
+    }
+
+    _closeBook(handle);
+  }
+
+  int getPageCount(Pointer<_MedicalCoreDocument> handle) {
+    if (handle == nullptr) {
+      throw ArgumentError('Document handle cannot be null.');
+    }
+
+    final pageCountPointer = calloc<Uint32>();
+
+    try {
+      final result = _getPageCount(handle, pageCountPointer);
+
+      if (result != 0) {
+        throw StateError('Failed to get PDF page count.');
+      }
+
+      return pageCountPointer.value;
+    } finally {
+      calloc.free(pageCountPointer);
+    }
+  }
+
+  MedicalCorePage renderPage(
+    Pointer<_MedicalCoreDocument> handle, {
+    required int pageIndex,
+    required int dpi,
+  }) {
+    if (handle == nullptr) {
+      throw ArgumentError('Document handle cannot be null.');
+    }
+
+    if (pageIndex < 0) {
+      throw ArgumentError.value(
+        pageIndex,
+        'pageIndex',
+        'Page index cannot be negative.',
+      );
+    }
+
+    if (dpi <= 0) {
+      throw ArgumentError.value(dpi, 'dpi', 'DPI must be greater than zero.');
+    }
+
+    final pagePointer = _renderPage(handle, pageIndex, dpi);
+
+    if (pagePointer == nullptr) {
+      throw StateError('Failed to render PDF page: $pageIndex');
+    }
+
+    try {
+      final page = pagePointer.ref;
+
+      final data = List<int>.from(page.data.asTypedList(page.dataLen));
+
+      return MedicalCorePage(
+        width: page.width,
+        height: page.height,
+        stride: page.stride,
+        components: page.components,
+        data: data,
+      );
+    } finally {
+      _freePage(pagePointer);
+    }
   }
 
   static DynamicLibrary _openLibrary() {
@@ -40,8 +272,6 @@ class MedicalCore {
       return DynamicLibrary.open('libmedical_core.so');
     }
 
-    throw UnsupportedError(
-      'MedicalCore is not supported on this platform.',
-    );
+    throw UnsupportedError('MedicalCore is not supported on this platform.');
   }
 }
