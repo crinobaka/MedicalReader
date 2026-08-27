856ca5b921e67b2cda48237dc79cb5abb5181cb5
crinobaka <crinobakabaka@qq.com>
2026-08-27 18:36:17 +0800

    0

--- tool/build_pdfx_windows.ps1
+++ tool/build_pdfx_windows.ps1
@@ -6,4 +6,5 @@ flutter pub get
 cd .\core\medical_core\
 cargo build
 cd ..\..
+Copy-Item -Path "D:\workshop\application\MedicalReader\core\medical_core\target\debug\medical_core.dll" -Destination "D:\workshop\application\MedicalReader\build\windows\x64\runner\Debug\"
 flutter run -d windows
