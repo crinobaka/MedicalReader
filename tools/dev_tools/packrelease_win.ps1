a53bb8e6910dd8ef0b49c01e942257ccc7e2d7a1
crinobaka <crinobakabaka@qq.com>
2026-08-30 09:02:51 +0800

    ls-tree

--- tools/dev_tools/packrelease_win.ps1
+++ tools/dev_tools/packrelease_win.ps1
@@ -6,6 +6,6 @@ flutter pub get
 cd .\core\medical_core\
 cargo build
 cd ..\..
-flutter run -d windows
+flutter bulid windows
 Copy-Item -Path "D:\workshop\application\MedicalReader\core\medical_core\target\debug\medical_core.dll" -Destination "D:\workshop\application\MedicalReader\build\windows\x64\runner\Release\"
 Compress-Archive -Path "build\windows\x64\runner\Release\*" -DestinationPath "MedicalReader_windows_x64_v1.0.0.zip" -Force
