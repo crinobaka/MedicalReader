c8737e82ce06393b2ff517d99469d0484ca52e63
crinobaka <crinobakabaka@qq.com>
2026-08-30 15:13:52 +0800

    local

---
+++ tool/packrelease_apk.ps1
@@ -0,0 +1,5 @@
+.\tool\build_medical_core_android.ps1
+flutter build apk
+
+# 重命名并移动（示例仅重命名 arm64）
+Rename-Item -Path ".\build\app\outputs\flutter-apk\app-release.apk" -NewName "MedicalReader_android_arm64-v8a_v1.5.0.alpha.apk"
