d7530212ec70be2e1a2ea0c7817be22b6a376504
crinobaka <105028523+crinobaka@users.noreply.github.com>
2026-09-13 16:33:43 +0800

    feat(reader): add PDF block reading public exports

---
+++ lib/features/reader/block/pdf_block_reading.dart
@@ -0,0 +1,8 @@
+export 'controllers/page_block_controller.dart';
+export 'models/page_block.dart';
+export 'services/page_block_manager.dart';
+export 'services/page_block_navigation.dart';
+export 'services/page_block_storage.dart';
+export 'widgets/page_block_editor.dart';
+export 'widgets/page_block_loading_overlay.dart';
+export 'widgets/page_block_viewport.dart';
